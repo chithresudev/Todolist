ad0833a9dcc9d838333a8768c6f0dd7fee5c9440
chithresudev <40000188+chithresudev@users.noreply.github.com>
2021-12-01 23:33:28 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
             $schedule->call(function () {
-               Task::whereNotNull('deleted_at')->delete();
+               Task::onlyTrashed()->forceDelete();
            })->monthly();
     }
 
